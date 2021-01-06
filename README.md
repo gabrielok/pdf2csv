5780846e7632d278fa07e0e40b62bae2bb0a75d9
Gabriel <gabrielokamoto@hotmail.com>
2021-01-06 19:50:53 -0300

    Todos quadros exportados para .csv

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 Teste 2 - pdf para csv
 
 Lógica do programa:
-1. Extrai as tabelas do pdf através da biblioteca tabula
+1. Extrai as tabelas do pdf através da biblioteca tabula, utilizando a posição na tela quando necessário
 2. Corrige as tabelas no caso de serem mal interpretadas
-3. Salva no formato .csv
+3. Salva uma por uma no formato .csv
+4. Compacta todos os .csv em um arquivo .zip
 
+Devido à grande quantidade de erros ao ler as tabelas do pdf (possivelmente devido a formatação inconsistente), elaborei uma coleção de funções _parse_ para padronizar a saída antes de exportar os dados. Caso mais quadros precisem ser extraídos no futuro, é possível reutilizar essas funções.
+
+Próximos passos:
+1. Converter os dados em float para int
+2. Obter automaticamente as coordenadas de tabelas no pdf para usar como argumento na leitura
 
 Referências:
 https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.drop.html
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import tabula, csv
+import tabula, csv, zipfile
 filename = 'Componente Organizacional 12.2020.pdf'
 
 def writecsv(file, df):
@@ -14,7 +14,7 @@ def readcsv(file):
         print(row)
 
 def parse1(df):
-    # reconstroi a tabela no caso de a coluna "código" ser lida junto à seguinte
+    # reconstrói a tabela no caso de a coluna "código" ser lida junto à seguinte
     # e os cabeçalhos serem interpretados como dados
     newcols = [[],[]]
     for idx, row in enumerate(df.values):
@@ -35,7 +35,38 @@ def parse1(df):
 
     nrows = df.shape[0]
     df.drop(nrows - 1, inplace = True) # apagar a última linha
-    df.drop(columns = q32[0].columns[-1], inplace = True) # apagar a última coluna
+    df.drop(columns = df.columns[-1], inplace = True) # apagar a última coluna
+
+def parse2(df):
+    # reconstrói a tabela no caso de os cabeçalhos serem interpretados como dados
+    cols = df.columns
+    df.rename(columns = {df.columns[0]: df.iloc[0,0], df.columns[1]: df.iloc[0,1]}, inplace = True)
+    df.drop(0, inplace = True)
+    return
+
+def parse3(df):
+    # reconstrói a tabela no caso de algumas linhas serem duplas
+    nuls = df.isnull()
+    nrows = df.shape[0]
+    i = 0
+    while i < nrows:
+        if nuls.iloc[i, 1]:
+            df.iloc[i-1, 0] = df.iloc[i, 0]
+            df.iloc[i-1, 1] = df.iloc[i-1, 1] + ' ' + df.iloc[i+1, 1]
+            df.drop(i, inplace = True)
+            df.drop(i+1, inplace = True)
+            df.reset_index(drop = True, inplace = True)
+            nuls.drop(i, inplace = True)
+            nuls.drop(i+1, inplace = True)
+            nuls.reset_index(drop = True, inplace = True)
+            nrows = nrows - 2
+        i = i + 1
+
+def append_dfs(df1, df2):
+    df2.columns = df1.columns
+    df1 = df1.append(df2)
+    df1.reset_index(inplace = True, drop = True)
+    return df1
 
 # Quadro 30
 q30 = tabula.read_pdf(filename, pages = 79, area = [16, 22, 26, 58], relative_area = True, silent = True, stream = True)
@@ -46,19 +77,33 @@ with open('q30.csv', 'w') as f:
     writecsv(f, q30[0])
 
 # Quadro 31
+# ler cada página isoladamente
 q31_1 = tabula.read_pdf(filename, pages = 79, area = [74, 22, 88, 84], relative_area = True, silent = True, stream = True)
-cols = q31_1[0].columns
-q31_1[0].rename(columns = {q31_1[0].columns[0]: q31_1[0].at[0,cols[0]], q31_1[0].columns[1]: q31_1[0].at[0,cols[1]]}, inplace = True)
-q31_1[0].drop(0, inplace = True)
+parse2(q31_1[0])
+
+q31_2 = tabula.read_pdf(filename, pages = '80-84',  silent = True, stream = True, pandas_options = {'columns': ['A', 'B']})
+for df in q31_2:
+    if df.isnull().values.any():
+        parse3(df)
+
+for df in q31_2:
+    # juntar as páginas
+    q31_1[0] = append_dfs(q31_1[0], df)
+    q31_1[0]
+q31 = q31_1[0]
+
 print('QUADRO 31')
-print(q31_1[0])
+print(q31.to_string())
+with open('q31.csv', 'w') as f:
+    writecsv(f, q31)
 
 # Quadro 32
 q32 = tabula.read_pdf(filename, pages = 85, area = [15, 22, 24, 53], relative_area = True, silent = True, stream = True)
+parse1(q32[0])
 print('QUADRO 32')
 print(q32[0])
-
-parse1(q32[0])
-
 with open('q32.csv', 'w') as f:
     writecsv(f, q32[0])
+
+# Zip dos arquivos
+filename = 'Teste_Intuitive_Care_Gabriel_Okamoto'
