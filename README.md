11bc1a61d640431238f6e71f4f4318d93868b0d1
Gabriel <gabrielokamoto@hotmail.com>
2021-01-06 17:22:33 -0300

    Quadros 30 e 32

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ Teste 2 - pdf para csv
 
 Lógica do programa:
 1. Extrai as tabelas do pdf através da biblioteca tabula
+2. Corrige as tabelas no caso de serem mal interpretadas
+3. Salva no formato .csv
+
+
+Referências:
+https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.drop.html
+https://nbviewer.jupyter.org/github/chezou/tabula-py/blob/master/examples/tabula_example.ipynb
+https://tabula-py.readthedocs.io/en/latest/tabula.html
--- main.py
+++ main.py
@@ -1,10 +1,49 @@
-import tabula
+import tabula, csv
 filename = 'Componente Organizacional 12.2020.pdf'
+
+def writecsv(file, df):
+    writer = csv.writer(file, delimiter=';', quotechar='"', lineterminator='\n')
+    header = [df.columns[0], df.columns[1]]
+    writer.writerow(header)
+    writer.writerows(df.values)
+    return
+
+def readcsv(file):
+    reader = csv.reader(file, delimiter=';', quotechar='"')
+    for row in reader:
+        print(row)
+
+def parse1(df):
+    # reconstroi a tabela no caso de a coluna "código" ser lida junto à seguinte
+    # e os cabeçalhos serem interpretados como dados
+    newcols = [[],[]]
+    for idx, row in enumerate(df.values):
+        # separar as colunas
+        sp = row[0].split()
+        newcols[0].append(sp[0])
+        newcols[1].append(' '.join(sp[1:]))
+
+    headers = []
+    for col in newcols:
+        # separar os cabeçalhos
+        headers.append(col.pop(0))
+        col.append('NaN')
+
+    for idx, col in enumerate(newcols):
+        # inserir as novas colunas
+        df.insert(idx, headers[idx], newcols[idx], True)
+
+    nrows = df.shape[0]
+    df.drop(nrows - 1, inplace = True) # apagar a última linha
+    df.drop(columns = q32[0].columns[-1], inplace = True) # apagar a última coluna
 
 # Quadro 30
 q30 = tabula.read_pdf(filename, pages = 79, area = [16, 22, 26, 58], relative_area = True, silent = True, stream = True)
 print('QUADRO 30')
 print(q30[0])
+
+with open('q30.csv', 'w') as f:
+    writecsv(f, q30[0])
 
 # Quadro 31
 q31_1 = tabula.read_pdf(filename, pages = 79, area = [74, 22, 88, 84], relative_area = True, silent = True, stream = True)
@@ -18,3 +57,8 @@ print(q31_1[0])
 q32 = tabula.read_pdf(filename, pages = 85, area = [15, 22, 24, 53], relative_area = True, silent = True, stream = True)
 print('QUADRO 32')
 print(q32[0])
+
+parse1(q32[0])
+
+with open('q32.csv', 'w') as f:
+    writecsv(f, q32[0])
